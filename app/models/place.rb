0237b54fca5939549fd5bdbc8ab3b8d52664d069
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-05-14 01:36:39 +0000

    Set up the bing  maps integration.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :address, :description, presence: true
   validates :name, presence: true, length: { minimum: 3 }
 end
