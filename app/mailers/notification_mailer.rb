379106ffce9f12fb002437f0c73eda49e1d5f697
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-05-26 16:55:29 +0000

    Configure email.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "christopher.duerkes@gmail.com",
+    mail(to: "duerkes.dev@gmail.com",
       subject: "A comment has been added to your place.")
   end
 
